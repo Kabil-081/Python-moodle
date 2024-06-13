74fb5b5be6786b2d4b0012dbd3a92cdbc0465233
Kabil-081 <231001081@rajalakshmi.edu.in>
2024-06-13 11:43:13 +0530

    Week-6.08.py

--- Week-6/Week-6.08.py
+++ Week-6/Week-6.08.py
@@ -1,5 +1,5 @@
 String characters balance Test
-
+.
 
 
 Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter. If balanced display as "true" ,otherwise "false".
