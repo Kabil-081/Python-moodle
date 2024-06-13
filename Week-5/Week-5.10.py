136e0b58526d2740db06a73e730a797a4b33600b
Kabil-081 <231001081@rajalakshmi.edu.in>
2024-06-13 11:41:44 +0530

    Week-5.10.py

--- Week-5/Week-5.10.py
+++ Week-5/Week-5.10.py
@@ -1,5 +1,5 @@
 Strictly increasing
-
+ 
 Write a Python program to check if a given list is strictly increasing or not. Moreover, If removing only one element from the list results in a strictly increasing list, we still consider the list true
 
 Input:
