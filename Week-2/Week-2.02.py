4b16b3a6c1828cad50098bc7e0cf630c2b4c82cc
Kabil-081 <231001081@rajalakshmi.edu.in>
2024-06-13 11:34:33 +0530

    Week-2.02.py

--- Week-2/Week-2.02.py
+++ Week-2/Week-2.02.py
@@ -1,5 +1,6 @@
 Doll Sings
 
+
 In London, every year during Dasara there will be a very grand doll show. People try to invent new dolls of different varieties. The best-sold doll's creator will be awarded with a cash prize. So people broke their heads to create dolls innovatively. Knowing this competition, Mr.Lokpaul tried to create a doll that sings only when an even number is pressed and the number should not be zero and greater than 100.
 
  IF Lokpaul wins print true, otherwise false.
